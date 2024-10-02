ae0e8ca8b87800d42f07d7b9d9a1326df21ed7c7
LouisMeM <ls.mamtchourouk@gmail.com>
2024-10-02 19:27:13 +0200

    Création du début de la page menu

--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,27 @@ const store = useMainStore();
 
 <template>
 
+    <div class="u-flex u-align-content-center u-justify-content-center u-flex-direction-column h100">
+
+    <NuxtLink to="game" class="play-button u-p15">Play</NuxtLink>
+
+      <div>
+        <NuxtLink to="parametrages">Paramétrages</NuxtLink>
+        <NuxtLink to="scores"> Scores</NuxtLink>
+      </div>
+
+    </div>
+
 </template>
 
 <style scoped lang="scss">
+
+.PlayButton {
+  background-color: #ffc67d;
+  border-radius: 4px;
+
+}
+
+
 
 </style>
