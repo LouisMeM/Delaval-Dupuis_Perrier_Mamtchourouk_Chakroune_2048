e743aa4590dc60651419bd3a35bb817dbb0bebad
LouisMeM <ls.mamtchourouk@gmail.com>
2024-11-20 19:28:45 +0100

    Page paramétrage devient la page d'accueil, changements sur les boutons

--- pages/game.vue
+++ pages/game.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 
+import {useMainStore} from "~/store";
+
+const store = useMainStore();
+
+const boardSize = computed(() => store.boardSize);
+const gameType = computed(() => store.gameType);
 
 
 </script>
 
 <template>
+
+    <p v-html="boardSize"></p>
+    <p v-html="gameType"></p>
 
 </template>
 
--- store/index.ts
+++ store/index.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useMainStore = defineStore('main', {
     state: () => ({
         gameType: '',
-        boardSize: '',
+        boardSize: 4,
     }),
 
     actions: {
